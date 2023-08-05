7bce6eda3d89136cf8c9d7530ac84f820bc2d9b6
vxyagr <hello.angkin@gmail.com>
2023-08-05 13:46:40 +0700

    buttons

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -50,14 +50,14 @@ const NavBar = () => {
             </button>
             <Link href={dashboardLink}>
               <a className="hidden md:block text-gray-600 hover:text-gray-800">
-                <button className="bg-gradient-to-r from-blue-500 rounded-xl to-purple-600 text-white font-bold py-2 px-4 leading-none tracking-tight hover:bg-left hover:shadow-xl hover:shadow-blue-400/20 active:scale-95 dark:text-gray-900 sm:text-base md:text-base transition duration-300 ease-in-out hover:bg-gradient-to-r hover:from-blue-400 hover:to-purple-500">
-                  Apps Coming Soon
+                <button className="bg-[#79D5C6] rounded-full font-medium text-white  py-2 px-4 leading-none tracking-tight hover:bg-left hover:shadow-xl hover:shadow-blue-400/20 active:scale-95 dark:text-gray-900 sm:text-base md:text-base transition duration-300 ease-in-out hover:bg-gradient-to-r hover:from-blue-400 hover:to-purple-500">
+                  <span className="text-[#101010]">Apps Coming Soon</span>
                 </button>
               </a>
             </Link>
           </div>
           {showMenu && (
-            <div className="md:hidden mt-4 space-y-4 p-5 text-center justify-center transition duration-300 ease-in-out">
+            <div className="md:hidden h-[100vh] bg-opacity-20 mt-4 space-y-4 p-5 text-center justify-center transition duration-300 ease-in-out">
               {/*<Link href="#howitworks">
                 <a className="block text-link">How It Works</a>
           </Link>*/}
@@ -76,8 +76,8 @@ const NavBar = () => {
               <div className="py-5">
                 <Link href={dashboardLink}>
                   <a className=" md:block text-gray-600 hover:text-gray-800 text-lg  py-8">
-                    <button className="bg-gradient-to-r from-blue-500 rounded-xl to-purple-600 text-white font-bold py-2  px-4 leading-none tracking-tight hover:bg-left hover:shadow-xl hover:shadow-blue-400/20 active:scale-95 dark:text-gray-900 sm:text-base md:text-base transition duration-300 ease-in-out hover:bg-gradient-to-r hover:from-blue-400 hover:to-purple-500">
-                      Apps Coming Soon
+                    <button className="bg-[#79D5C6] rounded-full text-white  py-2 px-4 leading-none tracking-tight hover:bg-left hover:shadow-xl hover:shadow-blue-400/20 active:scale-95 dark:text-gray-900 sm:text-base md:text-base transition duration-300 ease-in-out hover:bg-gradient-to-r hover:from-blue-400 hover:to-purple-500">
+                      <span className="text-[#101010]">Apps Coming Soon</span>
                     </button>
                   </a>
                 </Link>
