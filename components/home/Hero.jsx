5d96f0d049a974bd4fddd7aa8c3d2dce3790d34c
vxyagr <hello.angkin@gmail.com>
2023-08-05 11:53:17 +0700

    remove shadow

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -33,11 +33,11 @@ const HeroSection = () => {
         <div className="grid  gap-4 items-center h-full">
           <div className="text-white">
             <div className="lg:min-h-[150px]"></div>
-            <h1 className="text-3xl md:text-4xl lg:text-7xl font-bold text-center text-shadow-lg hero-lexend">
+            <h1 className="text-3xl md:text-4xl lg:text-7xl font-bold text-center  hero-lexend">
               Bitcoin at 30%++ discount rate
             </h1>
 
-            <h2 className="text-3xl p-10 md:text-4xl lg:text-7xl font-bold text-center text-shadow-lg ">
+            <h2 className="text-3xl p-10 md:text-4xl lg:text-7xl font-bold text-center  ">
               <Link href={"https://discord.gg/yDytXMMDs9"}>
                 <button className="bg-gradient-to-r from-blue-500 rounded-full to-purple-600 text-white lg:text-4xl md:text-2xl text-lg font-bold lg:py-6 lg:px-8 py-3 px-4 leading-none tracking-tight hover:bg-left hover:shadow-xl hover:shadow-blue-400/20 active:scale-95 sm:text-2xl  transition duration-300 ease-in-out hover:bg-gradient-to-r hover:from-blue-400 hover:to-purple-500">
                   Get Early Access
