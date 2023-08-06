de2cc5db90795519c0f3d6ee0886e30fef831fa3
vxyagr <hello.angkin@gmail.com>
2023-08-06 08:48:21 +0700

    button hover color

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -50,7 +50,7 @@ const NavBar = () => {
             </button>
             <Link href={dashboardLink}>
               <a className="hidden md:block text-gray-600 hover:text-gray-800">
-                <button className="bg-[#79D5C6] rounded-full font-medium text-white  py-2 px-4 leading-none tracking-tight hover:bg-left hover:shadow-xl hover:shadow-blue-400/20 active:scale-95 dark:text-gray-900 sm:text-base md:text-base transition duration-300 ease-in-out hover:bg-gradient-to-r hover:from-blue-400 hover:to-purple-500">
+                <button className="bg-[#79D5C6] rounded-full font-medium text-white  py-2 px-4 leading-none tracking-tight hover:bg-left hover:shadow-xl hover:shadow-blue-400/20 active:scale-95 sm:text-base md:text-base transition duration-300 ease-in-out hover:bg-[#cff0ea] ">
                   <span className="text-[#101010]">Apps Coming Soon</span>
                 </button>
               </a>
@@ -76,7 +76,7 @@ const NavBar = () => {
               <div className="py-5">
                 <Link href={dashboardLink}>
                   <a className=" md:block text-gray-600 hover:text-gray-800 text-lg  py-8">
-                    <button className="bg-[#79D5C6] rounded-full text-white  py-2 px-4 leading-none tracking-tight hover:bg-left hover:shadow-xl hover:shadow-blue-400/20 active:scale-95 dark:text-gray-900 sm:text-base md:text-base transition duration-300 ease-in-out hover:bg-gradient-to-r hover:from-blue-400 hover:to-purple-500">
+                    <button className="bg-[#79D5C6] rounded-full text-white  py-2 px-4 leading-none tracking-tight hover:bg-left hover:shadow-xl hover:shadow-blue-400/20 active:scale-95 dark:text-gray-900 sm:text-base md:text-base transition duration-300 ease-in-out hover:bg-[#cff0ea]">
                       <span className="text-[#101010]">Apps Coming Soon</span>
                     </button>
                   </a>
