bf7afee6c5bf849507fc46bbbfb38977a2fa634e
vxyagr <hello.angkin@gmail.com>
2023-09-29 12:12:22 +0700

    beta 2

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -58,7 +58,7 @@ const NavBar = () => {
             <Link href={dashboardLink}>
               <a className="hidden md:block text-gray-600 hover:text-gray-800">
                 <button className="bg-[#79D5C6] rounded-full font-medium text-white  py-2 px-4 leading-none tracking-tight hover:bg-left hover:shadow-xl hover:shadow-blue-400/20 active:scale-95 sm:text-base md:text-base transition duration-300 ease-in-out hover:bg-[#cff0ea] ">
-                  <span className="text-[#101010]">Testnet App</span>
+                  <span className="text-[#101010]">Try Beta</span>
                 </button>
               </a>
             </Link>
@@ -84,7 +84,7 @@ const NavBar = () => {
                 <Link href={dashboardLink}>
                   <a className=" md:block text-gray-600 hover:text-gray-800 text-lg  py-8">
                     <button className="bg-[#79D5C6] rounded-full text-white  py-2 px-4 leading-none tracking-tight hover:bg-left hover:shadow-xl hover:shadow-blue-400/20 active:scale-95 dark:text-gray-900 sm:text-base md:text-base transition duration-300 ease-in-out hover:bg-[#cff0ea]">
-                      <span className="text-[#101010]">Beta App</span>
+                      <span className="text-[#101010]">Try Beta</span>
                     </button>
                   </a>
                 </Link>
